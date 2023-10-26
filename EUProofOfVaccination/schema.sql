76b02c7e532b5eb0a45445928148ca82342dca47
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:04:44 +0200

    test of SQL export

--- EUProofOfVaccination/schema.sql
+++ EUProofOfVaccination/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model EUProofOfVaccination of the subject dataModel.COVID19 for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE EUProofOfVaccination_type AS ENUM ('EUProofOfVaccination');
-CREATE TABLE EUProofOfVaccination (dob TEXT, nam JSON, r JSON, t JSON, type EUProofOfVaccination_type, v JSON, ver TEXT);
+CREATE TABLE EUProofOfVaccination (dob TEXT, id TEXT PRIMARY KEY, nam JSON, r JSON, t JSON, type EUProofOfVaccination_type, v JSON, ver TEXT);
